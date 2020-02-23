7cb8c05a6b49050ff3045cd166129f19591bd775
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-02-23 09:53:48 +0100

    nfp121-tp11 added

---
+++ src/fr/cnam/nfp121/tp11/utils/TextColor.java
@@ -0,0 +1,19 @@
+package fr.cnam.nfp121.tp11.utils;
+
+
+public enum TextColor {
+
+    Highlight(ANSIColorCode.ANSI_WHITE_BACKGROUND),
+    DEFAULT(ANSIColorCode.ANSI_RESET),
+    RED(ANSIColorCode.ANSI_RED),
+    BLUE(ANSIColorCode.ANSI_BLUE),
+    GREEN(ANSIColorCode.ANSI_GREEN),
+    BLACK(ANSIColorCode.ANSI_BLACK),
+    YELLOW(ANSIColorCode.ANSI_YELLOW);
+
+    public final String set;
+
+    TextColor(String label) {
+        this.set = label;
+    }
+}
---
+++ src/fr/cnam/nfp121/tp11/utils/Tp11DebugOnOFF.java
@@ -0,0 +1,5 @@
+package fr.cnam.nfp121.tp11.utils;
+
+public final class Tp11DebugOnOFF {
+    public static final boolean DEBUG_ON = false;
+}
