29fc857acc776f8a1ea3457f9e85f308d62e671a
Nicolas HADDAD <nicolas.haddad80@google.com>
2020-02-29 18:37:13 +0100

    Debug set to false

--- src/fr/cnam/nfp121/tp13/DebugOnOFF.java
+++ src/fr/cnam/nfp121/tp13/DebugOnOFF.java
@@ -1,7 +1,7 @@
 package fr.cnam.nfp121.tp13;
 
 public final class DebugOnOFF {
-    public static final boolean DEBUG_ON = true;
+    public static final boolean DEBUG_ON = false;
     public static final int NB_DEBUG_PARALLEL_ADDITIONAL_VIEWS = 2;
     public static final String newline = System.getProperty("line.separator");
 }
