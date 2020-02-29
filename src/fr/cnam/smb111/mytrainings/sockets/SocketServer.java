0aaf8e7683455fbb767fb860d239a3ab4c30d464
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-02-29 19:29:23 +0100

    my first sockets communication

---
+++ src/fr/cnam/smb111/mytrainings/sockets/SocketServer.java
@@ -0,0 +1,21 @@
+package fr.cnam.smb111.mytrainings.sockets;
+
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+    public static void main(String[] args){
+        try{
+            ServerSocket ss=new ServerSocket(6666);
+            Socket s=ss.accept();//establishes connection
+            DataInputStream dis=new DataInputStream(s.getInputStream());
+            for(int i=0;i<100;i++) {
+                String str = (String) dis.readUTF();
+                System.out.println("message= " + str);
+            }
+            ss.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+}
