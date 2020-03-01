8336a541cce9949bca00351669a34d338fd890f8
Nicolas HADDAD <nicolas.haddad80@google.com>
2020-03-01 16:09:49 +0100

    commit from linux

--- src/fr/cnam/smb111/mytrainings/sockets/SocketClient.java
+++ src/fr/cnam/smb111/mytrainings/sockets/SocketClient.java
@@ -7,11 +7,8 @@ public class SocketClient {
     public static void main(String[] args) {
 
         try {
-<<<<<<< HEAD
-            Socket s = new Socket("localhost",2000);
-=======
             Socket s = new Socket("nicolas-haddad.hd.free.fr", 2000);
->>>>>>> 68651f33b52447992277f17dd22310937c737c9f
+
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             for(int messageNum=0;messageNum<100; messageNum++) {
                 StringBuilder message = new StringBuilder();
