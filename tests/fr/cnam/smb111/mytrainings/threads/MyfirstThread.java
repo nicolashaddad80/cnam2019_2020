0a851239154ea171f40b08892b28ca689b7b2c06
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-03-01 19:08:12 +0100

    My First Java Thread running

---
+++ tests/fr/cnam/smb111/mytrainings/threads/MyfirstThread.java
@@ -0,0 +1,11 @@
+package fr.cnam.smb111.mytrainings.threads;
+class MyfirstThread extends Thread{
+    public void run(){
+        System.out.println("thread is running...");
+    }
+    public static void main(String args[]){
+        MyfirstThread t1=new MyfirstThread();
+        t1.start();
+    }
+}
+
