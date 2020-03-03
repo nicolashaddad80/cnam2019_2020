723404d021292f6072ddd09563b37b7c02b0d78d
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-03-03 21:33:13 +0100

    Added Synchronisation for Server Counter Thread on system.out ressource

--- src/fr/cnam/smb111/cours02/tp2/CounterImpl.java
+++ src/fr/cnam/smb111/cours02/tp2/CounterImpl.java
@@ -1,20 +1,21 @@
 package fr.cnam.smb111.cours02.tp2;
 
 
+
 public class CounterImpl implements Counter {
     private int count = 0;
 
     @Override
     public  void increment() {
-        synchronized(this) {
+        synchronized(System.out) {
             this.count++;
         }
-
+        if (Debug.COUNTER_DEBUG_ON) System.out.println("New Counter Value is: " + this.count);
     }
 
     @Override
     public  void decrement() {
-        synchronized(this) {
+        synchronized(System.out) {
             this.count--;
             if (Debug.COUNTER_DEBUG_ON) System.out.println("New Counter Value is: " + this.count);
         }
