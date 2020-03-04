9504d2222fe6e61f58c3823fcae064418f99ab96
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-03-04 11:14:21 +0100

    Server Part done

--- src/fr/cnam/smb111/cours02/tp2_1/modeconnectemulticlients/Serveur.java
+++ src/fr/cnam/smb111/cours02/tp2_1/modeconnectemulticlients/Serveur.java
@@ -1,5 +1,7 @@
 package fr.cnam.smb111.cours02.tp2_1.modeconnectemulticlients;
 
+import fr.cnam.smb111.cours02.tp2_3.ServerParametersTp2_3;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,11 +17,11 @@ import java.net.Socket;
  */
 public class Serveur {
 
-    // Le num�ro de port est fixe ici arbitrairement a 5001
-    public static final int portEcoute = 5001;
+
+    public static final int portEcoute = ServerParametersTp2_3.SERVER_PORT_NUMBER;
 
     public static void main(String[] args) {
-        // Cr�ation de la socket serveur
+        // Création de la socket serveur
         ServerSocket socketServeur = null;
         try {
             socketServeur = new ServerSocket(Serveur.portEcoute);
@@ -33,20 +35,10 @@ public class Serveur {
             Socket socketClient;
             while (true) {
                 socketClient = socketServeur.accept();
-                ThreadConnexion t = new ThreadConnexion(socketClient);
-                System.out.println("Nouvelle Connexion");
-                t.start();
+                new ThreadConnexion(socketClient).start();
             }
         } catch (IOException e) {
             System.err.println("Erreur lors de l'attente d'une connexion : " + e);
-            System.exit(-1);
-        }
-
-        // Fermeture de la socket
-        try {
-            socketServeur.close();
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la fermeture de la socket : " + e);
             System.exit(-1);
         }
     }
---
+++ src/fr/cnam/smb111/cours02/tp2_3/ServerParametersTp2_3.java
@@ -0,0 +1,6 @@
+package fr.cnam.smb111.cours02.tp2_3;
+
+public class ServerParametersTp2_3 {
+    public static final int SERVER_PORT_NUMBER = 2000;
+    public static final String SERVER_ADDRESS = null;
+}
